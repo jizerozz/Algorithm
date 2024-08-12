392a7be51b11bf4835468df5bc6edf8afa227835
zxxzero <jizero27@gmail.com>
2024-08-13 02:12:11 +0900

    [level 1] Title: 햄버거 만들기, Time: 285.35 ms, Memory: 97.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,14 @@
+function solution(ingredient) {
+    var answer = 0;
+    const stack = []
+    
+    for(let i of ingredient){
+        stack.push(i)
+        if(stack.length>=4 && stack.slice(-4).toString() === '1,2,3,1'){
+            stack.splice(-4)
+            answer++
+        }
+    }
+    
+    return answer;
+}
