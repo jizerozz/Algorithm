4b3489db4685369496da9b13d62c81a168ac544e
zxxzero <jizero27@gmail.com>
2024-08-13 23:47:53 +0900

    [level 1] Title: 2016년, Time: 0.18 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,12 +1,9 @@
 function solution(a, b) {
-    var answer = '';
     let week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
     let date = new Date()
-    let year = date.setFullYear(2016);
-    let month = date.setMonth(a-1);
-    let day = date.setDate(b);
-    let dayofweek = date.getDay();
-    answer= week[dayofweek];
-
-    return answer;
+    let year = date.setFullYear(2016)
+    let month = date.setMonth(a-1)
+    let day = date.setDate(b)
+    let res = date.getDay()
+    return answer= week[res]
 }
--- 프로그래머스/1/12901. 2016년/README.md
+++ 프로그래머스/1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
 
 ### 성능 요약
 
-메모리: 34 MB, 시간: 31.69 ms
+메모리: 33.9 MB, 시간: 0.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 22일 15:19:07
+2024년 08월 13일 23:47:51
 
 ### 문제 설명
 
