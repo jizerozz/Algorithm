e2a024e11c4b8982740108a47bc7f4e43d780e42
zxxzero <jizero27@gmail.com>
2024-08-23 23:58:33 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -2,7 +2,7 @@ function solution(array, height) {
     var answer = 0;
     let count = 0
     array.forEach((el) => {
-        answer = Number(el) > height ? ++count : count
+        return Number(el) > height ? ++answer : answer
     })
     return answer;
 }
