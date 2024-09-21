c217fcc2cd8ccf7dc585806b1bf5af11c18e17cb
jizerozz <jizero27@gmail.com>
2024-09-21 23:48:06 +0900

    [level 2] Title: 귤 고르기, Time: 120.34 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -3,7 +3,6 @@ function solution(k, tangerine) {
     var answer = 0;
     const obj={}
     
-    //귤 종류와 갯수 구하기
     tangerine.forEach((t)=>{
         if(obj[t]){
             obj[t] = obj[t] + 1
@@ -13,7 +12,6 @@ function solution(k, tangerine) {
     })
     
     let cnt = Object.values(obj).sort((a,b)=>(b-a))
-    console.log(cnt)
    
     for(let n of cnt){
         result+=n
@@ -25,7 +23,3 @@ function solution(k, tangerine) {
    return answer;
 }
 
-
-//중복되는 tangerine 요소들만 구함
-//k만큼 담아냄
-//크기가 다른 종류의 수 최솟값 출력
