5bb21ca22af93e2c62d7b042b17f4a6fc4c92cd3
jizerozz <jizero27@gmail.com>
2024-10-27 22:29:29 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 2.17 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,22 @@
+function solution(board, moves) {
+    var answer = 0;
+    let basket = []
+        moves.forEach((move)=>{
+           const col = move -1
+            for(let row = 0 ; row<board.length; row++){
+                if(board[row][col] !== 0 ){
+                    const doll = board[row][col]
+                    board[row][col] = 0
+        
+                    if(basket[basket.length - 1] === doll){
+                        basket.pop()
+                        answer+=2
+                    }else{
+                        basket.push(doll)
+                    }
+                    break
+                }
+            }
+        })
+    return answer;
+}
