63385059bdd74d8e5131030b1bf96cb369eeea10
jizerozz <jizero27@gmail.com>
2025-04-21 20:38:26 +0900

    [level 1] Title: 덧칠하기, Time: 256.38 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,13 +1,4 @@
 function solution(n, m, section) {
-    var answer = 0
-    let painted = 0
-
-    for (let i = 0; i < section.length; i++) {
-        if(section[i] > painted ){
-            painted = section[i] + m - 1
-            answer++
-        }
-     }
+    var answer = 0;
     return answer;
 }
-
