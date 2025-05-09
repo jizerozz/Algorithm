0ad73f1cc5def0aeb29200e85e6a68ea0ba05760
jizerozz <jizero27@gmail.com>
2025-05-09 17:02:15 +0900

    [level 2] Title: 괄호 회전하기, Time: 29.96 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,29 +1,34 @@
 function solution(s) {
     var answer = 0;
-    let rotated = ''
+
     for(let i=0; i<s.length; i++){
-        rotated = s.slice(i) + s.slice(0,i)
-        if(isValid(rotated)) answer++
+        let str = ''
+        str = s.slice(i) + s.slice(0, i)
+        rotation(str) && answer++
     }
     
-    function isValid(str){
+    
+    function rotation(str){
         const stack = []
         for(let c of str){
-            if(c === '(' || c === '{' || c === '['){
+            if(c === '(' || c=== '[' || c === '{'){
                 stack.push(c)
             }else{
-                const last = stack.pop()
-                
-                if((c === ')' && last !== '(') || 
-                   (c === ']' && last !== '[') || 
-                   (c === '}'&& last!== '{')){
-                    return false
+                  const top = stack.pop()
+                if(
+                    top !== '(' && c === ')'
+                    || top !== '[' && c === ']'
+                    || top !== '{' && c === '}')
+                {
+                return false
                 }
             }
         }
         return stack.length === 0
     }
-
+   
     
     return answer;
 }
+
+// ([{
