fad86d7cd2304aed4338cf72f1b93f7834722a60
jizerozz <jizero27@gmail.com>
2025-05-09 16:24:21 +0900

    [level 2] Title: 올바른 괄호, Time: 27.79 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,16 @@
+function solution(s){
+   var answer = false
+   let stack = []
+   
+   for(let c of s){
+       if(c === '('){
+           stack.push(c)
+       }else{
+           if(stack.length === 0) return false
+           stack.pop()
+       }
+   }
+    return stack.length === 0
+}
+// )()
+//)(
