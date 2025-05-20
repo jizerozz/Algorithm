b8c68bb8b9d66f9bb4241b492537c098fe7d5cd4
jizerozz <jizero27@gmail.com>
2025-05-20 23:28:36 +0900

    [level 1] Title: K번째수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,6 +1,6 @@
 function solution(array, commands) {
     var answer = [];
-    answer = commands.map((el, idx)=>
+    answer = commands.map((el)=>
         array.slice(el[0]-1, el[1]).sort((a,b)=>a-b).at(el[2]-1)
     )
     return answer;
