b384b22ff2b85c0829b9c7a5d2a989e37a577497
jizerozz <jizero27@gmail.com>
2025-07-13 22:41:50 +0900

    [Bronze V] Title: 오늘 날짜, Time: 88 ms, Memory: 9296 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.js
@@ -0,0 +1,8 @@
+const today = new Date();
+console.log(
+  today.getFullYear() +
+    "-" +
+    String(today.getMonth() + 1).padStart(2, "0") +
+    "-" +
+    String(today.getDate()).padStart(2, "0")
+);
