378aea248216dd5c16b55c09b7a10b2821b72fef
jizerozz <jizero27@gmail.com>
2025-09-30 19:11:21 +0900

    [level 2] Title: 가장 큰 수, Time: 167.71 ms, Memory: 46 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+    var answer = '';
+    
+    answer = numbers.map(String).sort((a,b) => (b+a) - (a+b))
+    
+    return answer[0] === '0' ? '0' : answer.join('')
+    
+}
