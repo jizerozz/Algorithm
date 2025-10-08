533d94976db9a908c9580af14c80d835437d0f02
jizerozz <jizero27@gmail.com>
2025-10-08 21:21:54 +0900

    [level 1] Title: 2016년, Time: 60.25 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -2,6 +2,7 @@ function solution(a, b) {
     let week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
     let date = new Date()
     let year = date.setFullYear(2016)
+    console.log(date)
     let month = date.setMonth(a-1)
     let day = date.setDate(b)
     let res = date.getDay()
