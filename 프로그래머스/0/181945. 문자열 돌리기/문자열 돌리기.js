bacf05f81c00a51610e6301fe30f5cdd5ace1cb6
jizerozz <jizero27@gmail.com>
2025-11-03 21:30:55 +0900

    [level 0] Title: 문자열 돌리기, Time: 107.08 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    let words=str.split('');
+    for(let i in words){
+        console.log(words[i]);
+    }
+    
+});
