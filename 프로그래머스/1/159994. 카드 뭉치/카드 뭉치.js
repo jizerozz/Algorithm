28b49d8a33a0162169e9cbedeecd8d7236f970fa
jizerozz <jizero27@gmail.com>
2025-12-18 22:36:22 +0900

    [level 1] Title: 카드 뭉치, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,17 @@
+function solution(cards1, cards2, goal) {
+    var answer = 'Yes';
+    
+    
+    for(let word of goal){
+        if(word===cards1[0]){
+            cards1.shift();
+        }else if(word===cards2[0]){
+            cards2.shift();
+        }else{
+            return 'No';
+            
+        }
+    }
+    
+    return 'Yes';
+}
