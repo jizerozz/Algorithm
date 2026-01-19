89308a3b488989bd30b456bc046c2c5a31c6d1f7
jizerozz <jizero27@gmail.com>
2026-01-19 20:03:32 +0900

    [level 0] Title: 배열 조각하기, Time: 1.10 ms, Memory: 39.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -0,0 +1,9 @@
+function solution(arr, query) {
+    var answer = [];
+    
+    for(let i=0;i<query.length;i++){
+        i%2 ? arr.splice(0,query[i]) : arr.splice(query[i]+1);
+    }
+    
+    return arr;
+}
