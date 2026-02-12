2d3aed2bcfb13e76f0d1b937fccba8b2bc67eefb
jizerozz <jizero27@gmail.com>
2026-02-12 23:51:43 +0900

    [level 0] Title: 특이한 정렬, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,16 @@
+function solution(numlist, n) {
+    var answer = [];
+    
+    answer = numlist.sort((a,b) =>{
+        const diffA = Math.abs(a - n)
+        const diffB = Math.abs(b - n)
+        
+        if(diffA === diffB){
+            return b - a
+        }
+        
+        return diffA - diffB
+    } )
+    
+    return answer;
+}
