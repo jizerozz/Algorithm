c5f6fa36628542992a2da477f9a38b541f68ad3e
jizerozz <jizero27@gmail.com>
2026-04-29 22:13:26 +0900

    [level 3] Title: 네트워크, Time: 0.49 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -2,29 +2,31 @@ function solution(n, computers) {
     var answer = 0;
     let visited = Array(n).fill(false)
     
-    function dfs(computer){
-        visited[computer] = true
+    function dfs(node){
+        visited[node] = true
         
-        for(let i=0; i<n; i++){
-            if(computers[computer][i] === 0){
-                continue
+        for(let i=0; i<computers.length; i++){
+            if(computers[node][i] === 1){
+                if(!visited[i]){
+                    dfs(i)
+                }
             }
-            
-            if(visited[i]){
-                continue
-            }
-            dfs(i)
         }
     }
     
-    
     for(let i=0; i<n; i++){
+        //새로운 네트워크 발견
         if(!visited[i]){
             dfs(i)
             answer++
         }
     }
     
+    
+    
     return answer;
 }
 
+110
+110
+001
