4e5420e1d2e289c4fd07a54b6aebfe8f24419be6
jizerozz <jizero27@gmail.com>
2026-05-03 20:09:27 +0900

    [level 1] Title: 햄버거 만들기, Time: 287.13 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,15 +1,17 @@
 function solution(ingredient) {
     var answer = 0;
-    const stack = []
     
-    for(let i of ingredient){
-        stack.push(i)
-        if(stack.length>=4 && stack.slice(-4).toString() === '1,2,3,1'){
-            stack.splice(-4)
+    let storedBurger = []
+    
+    for(let i=0; i<ingredient.length; i++){
+        storedBurger.push(ingredient[i])
+        
+        if(storedBurger.length >= 4 && storedBurger.slice(-4).toString() === '1,2,3,1'){
+            storedBurger.splice(-4)
             answer++
         }
     }
-    console.log(stack)
+    
     
     return answer;
 }
